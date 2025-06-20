2b91b3a8121a1c02ed82ba977ff332f2c4f9dfe9
akrita01 <akrita.singh@student.adelaide.edu.au>
2025-06-20 12:15:15 +0000

    [automatic commit] 2025-06-20 12:15:14 UTC 129.127.202.33

--- part2/app.js
+++ part2/app.js
@@ -4,10 +4,19 @@ const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
-3
+
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+// Session setup
+app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'your_secret_key', // Use a secure key
+      resave: false,
+      saveUninitialized: true,
+    })
+  );
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
