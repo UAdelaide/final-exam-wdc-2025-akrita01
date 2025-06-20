afe8bc75aac7ffcc2420a0c74ce2e05426081435
akrita01 <akrita.singh@student.adelaide.edu.au>
2025-06-20 12:45:56 +0000

    [automatic commit] 2025-06-20 12:45:56 UTC 129.127.202.33

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -67,7 +67,6 @@ router.post('/login', async (req, res) => {
 router.post('/logout', (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      console.error('Error destroying session:', err);
       return res.status(500).json({ error: 'Logout failed' });
     }
     res.clearCookie('connect.sid'); // Clear session cookie
