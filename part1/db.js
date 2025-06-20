cc080dc98a2a1b5f199c2afb1f6658222d6b82d7
akrita01 <akrita.singh@student.adelaide.edu.au>
2025-06-20 10:40:31 +0000

    [automatic commit] 2025-06-20 10:40:31 UTC 129.127.202.33

--- part1/db.js
+++ part1/db.js
@@ -1,7 +1,15 @@
-const express = require('express');
-const mysql = require('mysql2/promise');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql2/promise');
 
-const app = express();
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 let db;
 
